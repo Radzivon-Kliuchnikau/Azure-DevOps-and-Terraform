4622eba88d080019d6702857fe2ec1b1fb50a9e2
Radzivon-Kliuchnikau <rodionk510@gmail.com>
2024-04-29 09:17:54 +0200

    Add terraform configs, improve dockerfile

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,5 @@
 # Build stage
 FROM mcr.microsoft.com/dotnet/sdk:8.0 AS build
-
 WORKDIR /source
 
 COPY . .
@@ -8,9 +7,8 @@ RUN dotnet restore "./weathertestapi/weathertestapi.csproj"
 RUN dotnet publish "./weathertestapi/weathertestapi.csproj" -c release -o /app --no-restore
 
 # Serve Stage
-FROM mcr.microsoft.com/dotnet/aspnet:8.0
+FROM mcr.microsoft.com/dotnet/aspnet:8.0-bookworm-slim-amd64 AS runtime
 WORKDIR /app
-ENV ASPNETCORE_ENVIRONMENT=Development
-EXPOSE 80
+EXPOSE 8080
 COPY --from=build /app ./
 ENTRYPOINT [ "dotnet", "weathertestapi.dll" ]
--- main.tf
+++ main.tf
@@ -15,5 +15,26 @@ provider "azurerm" {
 
 resource "azurerm_resource_group" "tf_test" {
   name     = "tfmainrg"
-  location = "Poland"
+  location = "polandcentral"
+}
+
+resource "azurerm_container_group" "trcg_test" {
+    name = "weathertestapi"
+    location = azurerm_resource_group.tf_test.location
+    resource_group_name = azurerm_resource_group.tf_test.name
+    ip_address_type = "Public"
+    dns_name_label = "rklrklweathertestapi"
+    os_type = "Linux"
+    restart_policy = "OnFailure"
+
+    container {
+        name = "weathertestapi"
+        image = "rklrkl/weathertestapi"
+        cpu = 1
+        memory = 1
+        ports {
+            port = 8080
+            protocol = "TCP"
+        }
+    }
 }
